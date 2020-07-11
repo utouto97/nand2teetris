43f85d1395b691cd8182587b4a0a20b31565f88d
utouto97 <utouto97@gmail.com>
2020-07-12 00:03:22 +0900

    update: rename .cpp to .h and create main.cpp

--- 10/Tokenizer.h
+++ 10/Tokenizer.h
@@ -109,26 +109,3 @@ struct Tokenizer {
 
   bool is_space(char c) { return c == ' ' or c == '\t'; }
 };
-
-int main(int argc, char* argv[]) {
-  string inputfile = argv[1];
-  for (const auto& entry : std::filesystem::directory_iterator(inputfile)) {
-    string p = entry.path();
-    if (p.substr(p.size() - 5, 5) != ".jack") continue;
-    string outfile = p.substr(0, p.size() - 5) + "_.xml";
-    ofstream ofs(outfile);
-    Tokenizer tokenizer(p);
-    auto tokens = tokenizer.analyze();
-    ofs << "<tokens>" << endl;
-    for (auto t : tokens) {
-      if (t.word == "<") t.word = "&lt;";
-      if (t.word == ">") t.word = "&gt;";
-      if (t.word == "\"") t.word = "&quot;";
-      if (t.word == "&") t.word = "&amp;";
-      ofs << "<" << t.type << "> " << t.word << " </" << t.type << ">" << endl;
-    }
-    ofs << "</tokens>" << endl;
-  }
-
-  return 0;
-}
